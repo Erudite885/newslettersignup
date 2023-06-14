c153a80be52c46a11fbcaae8abd9ce00edbe8701
uchebianca <erudite885@gmail.com>
2023-06-14 08:50:16 +0100

    asset renamed

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,7 @@
 import success from './icon-success.svg'
 import signupMobile from './illustration-sign-up-mobile.svg'
 import signupDesktop from './illustration-sign-up-desktop.svg'
-import iconlist from './iconlist.svg'
+import iconlist from './icon-list.svg'
 
 export {
     success,
